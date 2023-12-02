dba0ca54543cbcfa5d6d493ba72d1c9b03648afe
Serguiei Vassiliev <svassiliev1@gmail.com>
2023-12-02 16:34:41 -0400

    updated to version 5.5.0

--- sbatch/submit_busco.sh
+++ sbatch/submit_busco.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-#SBATCH --time 12:0:0 --mem-per-cpu=4000
+#SBATCH --time 3:0:0 --mem-per-cpu=4000
 
-module purge
-ml gcc busco
+source /project/def-orajora/software/env-busco/bin/activate
 
-busco -m transcriptome -i trinity_out_dir.Trinity.fasta -o BUSCO_OUTPUT -l busco_downloads/lineages/embryophyta_odb10 
+busco -m transcriptome\
+ --offline\
+ -i /project/def-orajora/svassili/BlackSpruce_Needle-Cambium/TRINITY/trinity_out_dir.Trinity.fasta\
+ -o BUSCO_OUTPUT\
+ -l /project/def-orajora/software/busco_downloads/lineages/embryophyta_odb10 
+
